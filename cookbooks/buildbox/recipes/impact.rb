83cd5874022d49d9f93e0e29d9af016cbb43d765
John Crocker <jcrocker@zenoss.com>
2013-10-07 12:15:27 -0500

    fix impact username
    CHange from zenoss_impact to zenossimpact to match rpm

--- cookbooks/buildbox/recipes/impact.rb
+++ cookbooks/buildbox/recipes/impact.rb
@@ -4,7 +4,7 @@ $profile_d_file = <<EOF
 #!/bin/bash
 user=`/usr/bin/id -un`
 case "$user" in
-    zenoss|zenoss_impact)
+    zenoss|zenossimpact)
         export IMPACT_HOME=/opt/zenoss_impact
         export PATH=$PATH:$IMPACT_HOME/bin
         ;;
@@ -14,21 +14,21 @@ case "$user" in
 esac
 EOF
 
-user "zenoss_impact" do
+user "zenossimpact" do
   action :create
 end
 group "zenoss" do
   action :modify
-  members ["zenoss_impact"]
+  members ["zenossimpact"]
 end
 directory "/opt/zenoss_impact" do
   owner "zenoss"
   group "zenoss"
   mode 0755
 end
-file "/etc/profile.d/zenoss_impact.sh" do
+file "/etc/profile.d/zenossimpact.sh" do
   content $profile_d_file
 end
-file "/etc/sudoers.d/zenoss_impact" do
-  content "zenoss_impact ALL=(ALL) ALL"
+file "/etc/sudoers.d/zenossimpact" do
+  content "zenossimpact ALL=(ALL) ALL"
 end
