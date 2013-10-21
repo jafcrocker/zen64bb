b4dd8e1f2614b3dc2c34890b8e69292ca289f7b4
John Crocker <jcrocker@zenoss.com>
2013-10-21 15:46:16 -0500

    Install JDK 7 for impact

--- cookbooks/buildbox/recipes/impact.rb
+++ cookbooks/buildbox/recipes/impact.rb
@@ -23,3 +23,28 @@ end
 file "/etc/profile.d/zenossimpact.sh" do
   content $profile_d_file
 end
+
+
+# Java
+$zen_download="http://vagrant.zendev.org/deps"
+$java_name="jdk-7u45-linux-x64.rpm"
+remote_file "#{$tmp_dir}/#{$java_name}" do
+  source  "#{$zen_download}/#{$java_name}"
+  action  :create_if_missing
+end
+execute "Install JDK 7.0" do
+    command "rpm -ivh --excludepath /etc/init.d/jexec #{$tmp_dir}/#{$java_name}"
+    not_if {::File.exists?("/usr/java/jdk1.7.0_45")}
+end
+link "/usr/java/default" do
+  to "/usr/java/jdk1.6.0_43"
+end
+link "/usr/java/zenoss_impact" do
+  to "/usr/java/latest"
+end
+file "/etc/default/zenoss_impact" do
+  content "JAVA_HOME=/usr/java/zenoss_impact"
+end
+
+
+
